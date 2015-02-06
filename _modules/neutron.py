412f783a55cb9aa9e51bfb315305e12b2ff0d8f7
florian.ermisch@fokus.fraunhofer.de <florian.ermisch@fokus.fraunhofer.de>
2015-02-06 15:55:27 +0100

    remove some unsupported parameters from router_update()

--- _modules/neutron.py
+++ _modules/neutron.py
@@ -416,11 +416,17 @@ def router_show(router_id): # name = None, router_id = None
     return neutron.show_router(router_id)
 
 def router_update(router_id, admin_state_up = None, 
-        network_id = None, tenant_id = None, subnet_id = None, 
-        port_id = None, new_name = None):
+        network_id = None, new_name = None):
     '''
-    Update parameters of given router
+    Update parameters of given router.
+
+    Optional parameters:
+    - admin_state_up
+    - network_id (has to be an external network!)
+    - new_name
     '''
+    # - Cannot update read-only attribute tenant_id
+    # 
     #if name is None and router_id is None:
     #    raise SaltInvocationError, 'You have to specify'\
     #        'at least one of "name" or "router_id".'
@@ -434,10 +440,6 @@ def router_update(router_id, admin_state_up = None,
         kwargs['external_gateway_info'] = { 'network_id': network_id }
     if tenant_id is not None:
         kwargs['tenant_id'] = tenant_id
-    if subnet_id is not None:
-        kwargs['subnet_id'] = subnet_id
-    if port_id is not None:
-        kwargs['port_id'] = port_id
     if new_name is not None:
         kwargs['name'] = new_name
     return neutron.update_router(router_id, {'router': kwargs})
