7c10b6a3814feacfbf39728552820f61318bf844
Florian Ermisch <florian.ermisch@tu-berlin.de>
2015-05-20 18:59:35 +0200

    neutron_router.managed: update supported parameters where needed
    these are:
        - admin_state_up
        - tenant_id
        - gateway_network
        - enable_snat
    
    still no ports or subnets.

--- _states/neutron_router.py
+++ _states/neutron_router.py
@@ -68,10 +68,51 @@ def managed(name, admin_state_up = None, tenant_id = None,
         log.debug('Found one router "{0}" '.format(name) +\
                 'with following properties: \n{0}'.format(
                     pp.pformat(router_list[0])))
+        router = router_list[0]
+        to_update = {}
+        if admin_state_up is not None and \
+                router.admin_state_up != admin_state_up:
+            to_update['admin_state_up'] = admin_state_up
+        if gateway_network is not None or enable_snat is not None:
+            if router['external_gateway_info']['network_id'] != \
+                    gateway_network or \
+                    router['external_gateway_info']['enable_snat'] != \
+                    enable_snat:
+                to_update['external_gateway_info'] = {
+                    'network_id': gateway_network,
+                    'enable_snat': enable_snat,
+                    }
         ret['comment'] = 'Router "{0}" already exists.'.format(name) +\
-                '\n(ID: {0})'.format(router_list[0]['id']) +\
-                '\n(Checking attributes not implemented yet!)'
-        # TODO: modify ret['changes'] as needed
+                '\n(ID: {0})'.format(router_list[0]['id'])
+        if to_update:
+            log.debug('Those properties need to be ' +\
+                'changed on router {0}: \n{1}'.format(name,
+                    pp.pformat(to_update)))
+            router_id = router.pop('id')
+            if to_update.has_key('external_gateway_info'):
+                ext_net_props = to_update.pop('external_gateway_info')
+                to_update['gateway_network'] = ext_net_props['network_id']
+                to_update['enable_snat'] = ext_net_props['enable_snat']
+            updated = __salt__['neutron.router_update'](
+                    router_id, **to_update)['router']
+            log.debug('Updated router {0}: \n{1}'.format(name, pp.pformat(
+                updated)))
+            for key in ['name', 'routes', 'status', 'tenant_id']:
+                if updated.has_key(key) and \
+                        router[key] != updated[key]:
+                    ret['changes'][key] = updated[key]
+            if updated.has_key('external_gateway_info'):
+                changed_gw_info = {}
+                if router['external_gateway_info']['network_id'] != \
+                        updated['external_gateway_info']['network_id']:
+                    changed_gw_info['network_id'] = \
+                        updated['external_gateway_info']['network_id']
+                if router['external_gateway_info']['enable_snat'] != \
+                        updated['external_gateway_info']['enable_snat']:
+                    changed_gw_info['enable_snat'] = \
+                        updated['external_gateway_info']['enable_snat']
+                ret['changes']['external_gateway_info'] = \
+                    changed_gw_info
     else:
         ret['result'] = False
         ret['comment'] = 'More than one router with this name already ' +\
